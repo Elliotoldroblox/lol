bc4055ff3f13bd13b37e76adb37706f2876de4f1
Elliotoldroblox <ejgumbs@gmail.com>
2025-08-01 19:29:22 +0200

    Create main.js
    lol

---
+++ .github/workflows/main.js
@@ -0,0 +1,27 @@
+function TheDifferentStagesOfGrowingUponyoutube() {
+  console.log("The different stages of growing up on YouTube");
+}
+TheDifferentStagesOfGrowingUponyoutube()
+var Thedifferentstagesofgrowingupurl = "https://www.youtube.com/shorts/BjRCRXw0W6s?feature=share"
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = {
+    TheDifferentStagesOfGrowingUponyoutube: TheDifferentStagesOfGrowingUponyoutube,
+    Thedifferentstagesofgrowingupurl: Thedifferentstagesofgrowingupurl
+  };
+}
+else {
+  window.TheDifferentStagesOfGrowingUponyoutube = TheDifferentStagesOfGrowingUponyoutube;
+  window.thedifferentstagesofgrowingupurl = Thedifferentstagesofgrowingupurl;
+}
+for (var i = 0; i < 5; i++) {
+  var name = "Elliot";
+    console.log("I love " + name);
+}
+JSON.stringify({ 'url': Thedifferentstagesofgrowingupurl });
+for (var i = 0; i < 100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000; i++) {
+    open("https://www.youtube.com/shorts/BjRCRXw0W6s?feature=share");
+}
+if (typeof window !== 'undefined') {
+  window.addEventListener('load', function() {
+    console.log("Page loaded and ready to go!");
+  });
