c888e3f1582cf77ed340ea6d7f0e6b84cc7fbc36
Elliotoldroblox <ejgumbs@gmail.com>
2025-08-08 15:56:56 +0200

    Update The different stages of growing up.js

--- The different stages of growing up.js
+++ The different stages of growing up.js
@@ -1,7 +1,7 @@
 function bookmark() {
   if ((navigator.appName == "Microsoft Internet Explorer") && (parseInt(navigator.appVersion) >= 4)) {
-    var url = "https://www.youtube.com/shorts/BjRCRXw0W6s?feature=share";
-    var title = "The different stages of growing up,";
+    var url = "https://www.youtube.com/shorts/2k2JjdWSNOQ?feature=share";
+    var title = "BEST CURSED LEGO  pieces...I I Build a LEGO";
 
     window.external.AddFavorite(url, title);
   }
